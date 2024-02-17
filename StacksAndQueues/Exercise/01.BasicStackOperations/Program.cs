62a0ec2f6df6f4ff8f86386fc44f231ef88284c4
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-17 12:08:16 +0200

    Add files via upload

---
+++ StacksAndQueues/Exercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] NSX = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int[] inputNumbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+Stack<int> stack = new Stack<int>();
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+    stack.Push(inputNumbers[i]);
+}
+if (stack.Count >= NSX[1])
+{
+    for (int i = 0; i < NSX[1]; i++)
+    {
+        stack.Pop();
+    }
+}
+
+if (stack.Contains(NSX[2]))
+{
+    Console.WriteLine("true");
+}
+else if (stack.Any())
+{
+    Console.WriteLine(stack.Min());
+}
+else
+{
+    Console.WriteLine(0);
+}
---
+++ StacksAndQueues/Exercise/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] NSX = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+if (queue.Count >= NSX[1])
+{
+    for (int i = 0; i < NSX[1]; i++)
+    {
+        queue.Dequeue();
+    }
+}
+
+if (!queue.Any())
+{
+    Console.WriteLine(0);
+}
+else if (queue.Contains(NSX[2]))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine(queue.Min());
+}
