c553ffa1559790c6def531dfcdd99dc15204095b
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-17 12:43:01 +0200

    Add files via upload

---
+++ SetsAndDictionaries/05.CountSymbols/Program.cs
@@ -0,0 +1,16 @@
+﻿char[] text = Console.ReadLine().ToArray();
+Dictionary<char, int> symbols = new Dictionary<char, int>();
+
+foreach (char c in text)
+{
+    if (!symbols.ContainsKey(c))
+    {
+        symbols.Add(c, 0);
+    }
+    symbols[c]++;
+}
+
+foreach (var c in symbols.OrderBy(c => c.Key))
+{
+    Console.WriteLine($"{c.Key}: {c.Value} time/s");
+}
---
+++ SetsAndDictionaries/06.Wardrobe/Program.cs
@@ -0,0 +1,41 @@
+﻿Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+int rows = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rows; i++)
+{
+    string[] row = Console.ReadLine().Split(" -> ").ToArray();
+    string color = row[0];
+    string[] clothes = row[1].Split(",").ToArray();
+
+    if (!wardrobe.ContainsKey(color))
+    {
+        wardrobe.Add(color, new Dictionary<string, int>());
+    }
+
+    foreach (string cloth in clothes)
+    {
+        if (!wardrobe[color].ContainsKey(cloth))
+        {
+            wardrobe[color].Add(cloth, 0);
+        }
+        wardrobe[color][cloth]++;
+    }
+}
+
+string[] pairToFind = Console.ReadLine().Split();
+string colorOfClothToFind = pairToFind[0];
+string clothTofind = pairToFind[1];
+foreach (var color in wardrobe)
+{
+    Console.WriteLine($"{color.Key} clothes:");
+    foreach (var cloth in color.Value)
+    {
+        Console.Write($"* {cloth.Key} - {cloth.Value}");
+
+        if (color.Key == colorOfClothToFind && cloth.Key == clothTofind)
+        {
+            Console.Write(" (found!)");
+        }
+        Console.WriteLine();
+    }
+}
