bd688d3e70a820196c04e90297cb82365be2798d
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-17 12:45:41 +0200

    Add files via upload

---
+++ StreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LineNumbers
+{
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int letterNumber = lines[i].Where(char.IsLetter).Count();
+                int punctiationMarks = lines[i].Where(char.IsPunctuation).Count();
+
+
+                lines[i] = $"Line {i + 1}: {lines[i]} ({letterNumber}) ({punctiationMarks})";
+                Console.WriteLine(lines[i]);
+            }
+
+            File.WriteAllLines(outputFilePath, lines);
+        }
+    }
+}
