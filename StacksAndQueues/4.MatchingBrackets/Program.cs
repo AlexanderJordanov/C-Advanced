7758026264888fd6e207b272c29dc74280a2aca6
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-17 12:04:41 +0200

    Add files via upload

---
+++ StacksAndQueues/4.MatchingBrackets/Program.cs
@@ -0,0 +1,15 @@
+﻿string expression = Console.ReadLine();
+Stack<int> brackets = new Stack<int>();
+
+for (int i = 0; i < expression.Length; i++)
+{
+    if (expression[i] == '(')
+    {
+        brackets.Push(i);
+    }
+    if (expression[i] == ')')
+    {
+        int startingIndex = brackets.Pop();
+        Console.WriteLine(expression.Substring(startingIndex, i - startingIndex + 1));
+    }
+}
---
+++ StacksAndQueues/6.Supermarket/Program.cs
@@ -0,0 +1,17 @@
+﻿Queue<string> queue = new Queue<string>();
+string customer = "";
+while ((customer = Console.ReadLine()) != "End")
+{
+    if (customer == "Paid")
+    {
+        while (queue.Count > 0)
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+        continue;
+    }
+
+    queue.Enqueue(customer);
+    ;
+}
+Console.WriteLine($"{queue.Count} people remaining.");
