bdca3f1fd0e1f654580f9e05fda024a33f76284f
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-17 12:05:53 +0200

    Add files via upload

---
+++ StacksAndQueues/7.HotPotato/Program.cs
@@ -0,0 +1,20 @@
+﻿Queue<string> kids = new Queue<string> (Console.ReadLine()
+    .Split()
+    .ToArray());
+int throws = int.Parse(Console.ReadLine());
+int currentThrows = 1;
+while (kids.Count > 1)
+{
+    string currentKid = kids.Dequeue();
+    if (currentThrows < throws)
+    {
+        kids.Enqueue(currentKid);
+        currentThrows++;
+    }
+    else
+    {
+        currentThrows = 1;
+        Console.WriteLine($"Removed {currentKid}");
+    }
+}
+Console.WriteLine($"Last is {kids.Dequeue()}");
---
+++ StacksAndQueues/8.TrafficJam/Program.cs
@@ -0,0 +1,24 @@
+﻿int carsToPass = int.Parse(Console.ReadLine());
+Queue<string> carsWaiting = new Queue<string>();
+int carCount = 0;
+string input = string.Empty;
+while ((input = Console.ReadLine()) != "end")
+{
+    if (input == "green")
+    {
+        for (int i = 0; i < carsToPass; i++)
+        {
+            if (carsWaiting.Count > 0)
+            {
+                Console.WriteLine($"{carsWaiting.Dequeue()} passed!");
+                carCount++;
+            }        
+        }
+    }
+    else
+    {
+        carsWaiting.Enqueue(input);
+    }
+}
+
+Console.WriteLine($"{carCount} cars passed the crossroads.");
