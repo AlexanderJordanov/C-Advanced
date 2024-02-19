8cdf49cdf9d4ab841689ef292559dd788c376c7e
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-19 18:47:49 +0200

    Add files via upload

---
+++ DefiningClasses/03.CarConstructors/Car.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+            :this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+            set
+            {
+                make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+        public double FuelQuantity
+        {
+            get
+            {
+                return fuelQuantity;
+            }
+            set
+            {
+                fuelQuantity = value;
+            }
+        }
+        public double FuelConsumption
+        {
+            get
+            {
+                return fuelConsumption;
+            }
+            set
+            {
+                this.fuelConsumption = value;
+            }
+        }
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity > distance * this.FuelConsumption)
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
