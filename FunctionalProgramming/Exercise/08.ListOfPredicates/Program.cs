9ff5a2b59be99e4458448c44f2502f8a8a5760c3
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-19 18:44:47 +0200

    Add files via upload

---
+++ FunctionalProgramming/Exercise/08.ListOfPredicates/Program.cs
@@ -0,0 +1,31 @@
+﻿int end = int.Parse(Console.ReadLine());
+int[] divisors = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Func<int[], int, bool> predicate = (array, i) =>
+{
+    bool isDivisble = true;
+    {
+        foreach (int divisor in divisors)
+        {
+            if (i % divisor != 0)
+            {
+                isDivisble = false;
+            }
+        }
+        return isDivisble;
+    }
+};
+
+List<int> numbers = new List<int>();
+for (int i = 1; i <= end; i++)
+{
+    if (predicate(divisors, i))
+    {
+        numbers.Add(i);
+    }
+}
+
+Console.WriteLine(string.Join(" ", numbers));
---
+++ FunctionalProgramming/Exercise/09.PredicateParty!/Program.cs
@@ -0,0 +1,61 @@
+﻿List<string> names = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+string input = default;
+
+while ((input = Console.ReadLine()) != "Party!")
+{
+    string[] command = input.Split();
+    Func<string, bool> predicate = GetPredicate(command[1], command[2]);
+
+    if (command[0] == "Remove")
+    {
+        names = Remove(names, predicate);
+    }
+    else if (command[0] == "Double")
+    {
+        names = Double(names, predicate);
+    }
+}
+
+Console.WriteLine(names.Any() ?
+    $"{string.Join(", ", names)} are going to the party!" :
+    "Nobody is going to the party!");
+
+Func<string, bool> GetPredicate(string command, string criteria)
+{
+    if (command == "StartsWith")
+    {
+        return s => s.StartsWith(criteria);
+    }
+    else if (command == "EndsWith")
+    {
+        return s => s.EndsWith(criteria);
+    }
+    else if (command == "Length")
+    {
+        return s => s.Length == int.Parse(criteria);
+    }
+
+    return default;
+}
+
+static List<string> Double (List<string> names, Func<string, bool> predicate)
+{
+    List<string> result = new List<string>();
+    foreach (string name in names)
+    {
+        if (predicate(name))
+        {
+            result.Add(name);
+        }
+        result.Add(name);
+    }
+    return result;
+}
+
+static List<string> Remove (List<string> names, Func<string, bool> predicate)
+{
+    return names.Where(name => predicate(name) == false).ToList();
+}
