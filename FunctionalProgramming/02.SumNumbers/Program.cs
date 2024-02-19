6355bc22a9972c0d69beb93d8e8c480300b7916b
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-19 18:38:01 +0200

    Add files via upload

---
+++ FunctionalProgramming/02.SumNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+Func<string, int> parser = n => int.Parse(n);
+
+int[] numbers = input
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(parser)
+    .ToArray();
+
+Console.WriteLine(numbers.Count());
+Console.WriteLine(numbers.Sum());
---
+++ FunctionalProgramming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,11 @@
+﻿Predicate<string> predicate = n  => n[0] == n.ToUpper()[0];
+
+string[] words = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Where(w => predicate(w))
+    .ToArray();
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
