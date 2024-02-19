33132c089940b4aa0cf7ce367868143491480c62
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-02-19 18:46:02 +0200

    Add files via upload

---
+++ FunctionalProgramming/Exercise/10.ThePartyReservationFilterModule/Program.cs
@@ -0,0 +1,58 @@
+﻿List<string> names = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+List<string> filters = new();
+string input = default;
+
+while ((input = Console.ReadLine()) != "Print")
+{
+    string[] commandArgs = input.Split(';');
+    string command = commandArgs[0];
+    string filterType = commandArgs[1];
+    string filterParameter = commandArgs[2];
+
+    if (command == "Add filter")
+    {
+        filters.Add(filterType + " " + filterParameter);
+    }
+    else if (command == "Remove filter")
+    {
+        filters.Remove(filterType + " " + filterParameter);
+    }
+}    
+
+foreach(string filter in filters)
+{
+    string[] commandArgs = filter.Split(' ');
+    
+    string filterType = commandArgs[0];
+    
+
+    if (filterType == "Starts")
+    {
+        string filterParameter = commandArgs[2];
+        names = names.Where(name => !name.StartsWith(filterParameter)).ToList();
+    }
+    else if (filterType == "Ends")
+    {
+        string filterParameter = commandArgs[2];
+        names = names.Where(name => !name.EndsWith(filterParameter)).ToList();
+    }
+    else if (filterType == "Length")
+    {
+        string filterParameter = commandArgs[1];
+        names = names.Where(name => name.Length != int.Parse(filterParameter)).ToList();
+
+    }
+    else if (filterType == "Contains")
+    {
+        string filterParameter = commandArgs[1];
+        names = names.Where(name => !name.Contains(filterParameter)).ToList();
+    }
+}
+
+if (names.Any())
+{
+    Console.WriteLine(string.Join(" ", names));
+}
---
+++ FunctionalProgramming/Exercise/11.TriFunction/Program.cs
@@ -0,0 +1,13 @@
+﻿int minSum = int.Parse(Console.ReadLine());
+List<string> names = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+Func<string, bool> predicate = name => name.Sum(c => c) >= minSum;
+foreach (string name in names)
+{
+    if (predicate(name))
+    {
+        Console.WriteLine(name);
+        break;
+    }
+}
